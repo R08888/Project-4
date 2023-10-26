9ed78f62fc8bf99f7584d17142851ea0bc837f56
RORY ARYA PERMANA <88544095+R08888@users.noreply.github.com>
2023-10-26 22:31:17 +0700

    Add files via upload

---
+++ espGPS/espGPS.ino
@@ -0,0 +1,27 @@
+
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+//SoftwareSerial serial_gps(2, 3); 
+#define serial_gps Serial1
+TinyGPSPlus gps;
+
+void setup() {
+  Serial.begin(9600);
+//  serial_gps.begin(9600);
+serial_gps.begin(9600, SERIAL_8N1, 27, 26);
+  Serial.println("GPS Mulai");
+}
+
+void loop() {
+  
+  while(serial_gps.available()) {
+    gps.encode(serial_gps.read());
+  }
+  if(gps.location.isUpdated()) {
+    Serial.print("Latitude=");
+    Serial.print(gps.location.lat(),6);
+    Serial.print(" Longitude=");
+    Serial.println(gps.location.lng(),6);
+    delay(2000);
+  }
+}
